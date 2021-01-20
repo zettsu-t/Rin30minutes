fe4497e1486f8ade4ca96eb3f830ff1cb1248261
Zettsu Tatsuya <zettsu.tatsuya@gmail.com>
2021-01-20 11:24:34 +0000

    Use assertthat::are_equal

--- list_packages.sh
+++ list_packages.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/bash
 echo "Imported packages in r_in_30minutes.Rmd"
-egrep library r_in_30minutes.Rmd | awk '!seen[$0]++' | sed -e s/library/install.packages/g -e 's/[(]/("/g' -e 's/[)]/")/g'
+egrep "library\\W" r_in_30minutes.Rmd | tr -d \\r | awk '!seen[$0]++' | sed -e s/library/install.packages/g -e 's/[(]/("/g' -e 's/[)]/")/g'
 echo
 
 echo "Imported packages in r_in_30minutes.Rmd (ordered)"
-egrep library r_in_30minutes.Rmd | sort -f | uniq | sed -e s/library/install.packages/g -e 's/[(]/("/g' -e 's/[)]/")/g'
+egrep "library\\W" r_in_30minutes.Rmd | tr -d \\r | sort -f | uniq | sed -e s/library/install.packages/g -e 's/[(]/("/g' -e 's/[)]/")/g'
 echo
 
 echo "R and C++ package:: names in r_in_30minutes.Rmd"
-egrep -o "\\b(\\w*::)" r_in_30minutes.Rmd | sort | uniq
+egrep -o "\\b(\\w*::)" r_in_30minutes.Rmd | tr -d \\r | sort | uniq
 echo
-
